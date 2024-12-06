b91124be1c90e708c27e719eda7c8536f5fb9b5f
jinsb1995 <jinsb1995@gmail.com>
2024-12-06 19:34:14 +0900

    chore: api문서 controller추가

---
+++ src/main/java/com/wirebarley/presentation/docs/ApiDocsController.java
@@ -0,0 +1,13 @@
+package com.wirebarley.presentation.docs;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApiDocsController {
+
+    @GetMapping("/api-docs")
+    public String apiDocs() {
+        return "docs/index.html";
+    }
+}
